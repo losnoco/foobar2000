c38f989e29b010b524c1e596fd40a79fcedf3c95
Chris Moeller <kode54@gmail.com>
2011-02-14 13:47:08 +0000

    Removed unnecessary function.
    git-tfs-id: [http://localhost:8080/tfs/DefaultCollection/]$/foobar2000/files/plugins.root;C635

--- plugins/foobar/foobar2000/foo_snarl/foo_snarl.cpp
+++ plugins/foobar/foobar2000/foo_snarl/foo_snarl.cpp
@@ -69,15 +69,6 @@ enum FSMsgClass : int {
 //	Stop,
 //};
 
-inline char base64_char(unsigned char in)
-{
-	static const char base64_chars[64] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
-										  'Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f',
-										  'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
-										  'w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/'};
-	return base64_chars[in];
-}
-
 void FSRegisterClass(int intClass){
 	bool error;
 	if(using_v42)
