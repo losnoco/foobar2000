1227d135c49a9599fe5eb90eb410062a021c6fa2
Chris Moeller <kode54@gmail.com>
2011-12-23 19:18:24 +0000

    Import missing residfp source files
    git-tfs-id: [http://localhost:8080/tfs/DefaultCollection/]$/foobar2000/files/plugins.root;C751

---
+++ plugins/sidplay-residfp/trunk/libsidplayfp/sidplayfp/sidversion.h
@@ -0,0 +1,14 @@
+#ifndef LIBSIDPLAYFP_VERSION_H
+#define LIBSIDPLAYFP_VERSION_H
+
+#ifndef _sidplay2_h_
+#  error Do not include directly.
+#endif
+
+
+
+#define LIBSIDPLAYFP_VERSION_MAJ 2
+#define LIBSIDPLAYFP_VERSION_MIN 0
+#define LIBSIDPLAYFP_VERSION_LEV 0
+
+#endif
