6df836393541bb128b9f0a07c00a27d40187cea9
Chris Moeller <kode54@gmail.com>
2012-07-03 01:04:13 -0700

    Added 6.1 and 7.1 channel flag definitions for Xiph channel guessing table

--- plugins/foobar/foobar2000/SDK/audio_chunk_channel_config.cpp
+++ plugins/foobar/foobar2000/SDK/audio_chunk_channel_config.cpp
@@ -95,6 +95,8 @@ static const unsigned g_audio_channel_config_table_xiph[] =
 	audio_chunk::channel_front_left | audio_chunk::channel_front_right | audio_chunk::channel_back_left | audio_chunk::channel_back_right,
 	audio_chunk::channel_front_left | audio_chunk::channel_front_right | audio_chunk::channel_back_left | audio_chunk::channel_back_right | audio_chunk::channel_front_center,
 	audio_chunk::channel_config_5point1,
+	audio_chunk::channel_front_left | audio_chunk::channel_front_right | audio_chunk::channel_front_center | audio_chunk::channel_lfe | audio_chunk::channel_back_center | audio_chunk::channel_front_center_right | audio_chunk::channel_front_center_left,
+	audio_chunk::channel_front_left | audio_chunk::channel_front_right | audio_chunk::channel_back_left | audio_chunk::channel_back_right | audio_chunk::channel_front_center | audio_chunk::channel_lfe | audio_chunk::channel_front_center_right | audio_chunk::channel_front_center_left,
 };
 
 unsigned audio_chunk::g_guess_channel_config(unsigned count)
