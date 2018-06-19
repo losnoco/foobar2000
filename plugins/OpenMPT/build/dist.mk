dcc233eca0fa2cabe494ec11be5fb875bab41bf7
Christopher Snowhill <kode54@gmail.com>
2018-06-18 19:07:05 -0700

    Updated libopenmpt to version 0.3.10.

--- plugins/OpenMPT/build/dist.mk
+++ plugins/OpenMPT/build/dist.mk
@@ -1,4 +1,4 @@
 
-MPT_SVNVERSION=10169
-MPT_SVNURL=https://source.openmpt.org/svn/openmpt/tags/libopenmpt-0.3.9
-MPT_SVNDATE=2018-04-29T06:08:50.600539Z
+MPT_SVNVERSION=10492
+MPT_SVNURL=https://source.openmpt.org/svn/openmpt/tags/libopenmpt-0.3.10
+MPT_SVNDATE=2018-06-17T05:53:11.604200Z
