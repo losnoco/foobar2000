f2a050d34fecc67e34cf55ae75c6eaa31f079437
Christopher Snowhill <kode54@gmail.com>
2019-02-11 01:17:59 -0800

    Updated libopenmpt to version 0.4.3

--- plugins/OpenMPT/build/dist.mk
+++ plugins/OpenMPT/build/dist.mk
@@ -1,4 +1,4 @@
 
-MPT_SVNVERSION=11243
-MPT_SVNURL=https://source.openmpt.org/svn/openmpt/tags/libopenmpt-0.4.2
-MPT_SVNDATE=2019-01-22T08:59:00.771884Z
+MPT_SVNVERSION=11358
+MPT_SVNURL=https://source.openmpt.org/svn/openmpt/tags/libopenmpt-0.4.3
+MPT_SVNDATE=2019-02-11T05:32:52.673531Z
